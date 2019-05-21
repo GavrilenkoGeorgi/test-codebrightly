37d5ddc1d53418bfcf1cc63dc2a2e21336e32942
Gavrilenko <gavrilenko.georgi@gmail.com>
2019-05-21 11:19:32 +0300

    Home button component added

--- src/components/homeButton.vue
+++ src/components/homeButton.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="lower-part">
+  <div class="title">
     <p>Lorem ipsum is text of the typesetting industry</p>
     <button
       class="home-button"
       @click="navigate"
     >
-      x-- Back to homepage
+      <backArrowIcon class="back-icon" />Back to homepage
     </button>
   </div>
 </template>
 
 <script>
+import backArrowIcon from '@/assets/svg/backArrowIcon.svg'
+
 export default {
   name: 'homeButton',
+  components: {
+    backArrowIcon
+  },
   methods: {
     navigate () {
       this.$router.push('/')
@@ -22,15 +27,22 @@ export default {
 </script>
 
 <style lang="sass" scoped>
-.lower-part
-  text-align: center
+.title
+  display: flex
+  flex-direction: column
+  align-items: center
   padding: 2em 0em 2em 0em
   p
-    font-size: 1.3em
+    font-size: 1.4em
+    color: gray
   .home-button
     background-color: white
     border: none
     border: .075em solid #60e3a1
     height: 3em
     border-radius: .2em
+    padding: 0em 1em 0em 1em
+    font-size: 1em
+    .back-icon
+      padding-right: .6em
 </style>
